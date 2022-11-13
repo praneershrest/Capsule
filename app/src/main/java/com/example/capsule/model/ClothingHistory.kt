1ebd74d60f21f5d56b88603a1f4f148d144782f7
Chang <47656782+changyyn@users.noreply.github.com>
2022-11-13 13:51:33 -0800

    Update build.gradle to include room database and coroutines, models for Database

---
+++ app/src/main/java/com/example/capsule/model/ClothingHistory.kt
@@ -0,0 +1,17 @@
+package com.example.capsule.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "clothing_history_table")
+class ClothingHistory (
+    @PrimaryKey(autoGenerate = true)
+    val id : Long = 0L,
+
+    @ColumnInfo(name="clothing_id")
+    val clothingId : Long = 0L,
+
+    @ColumnInfo(name="date")
+    val date : String = ""
+    )
