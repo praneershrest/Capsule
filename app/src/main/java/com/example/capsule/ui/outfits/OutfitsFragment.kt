469d9281765354e205722cda733663480a36825c
Denzel Tjokroardi <dtjokroa@gmail.com>
2022-11-27 19:14:22 -0800

    Rebased to dev

--- app/src/main/java/com/example/capsule/ui/outfits/OutfitsFragment.kt
+++ app/src/main/java/com/example/capsule/ui/outfits/OutfitsFragment.kt
@@ -1,17 +1,33 @@
 package com.example.capsule.ui.outfits
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.capsule.Util
+import com.example.capsule.api.WeatherApi
 import com.example.capsule.databinding.FragmentOutfitsBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.android.gms.tasks.Task
 
 class OutfitsFragment : Fragment() {
 
     private var _binding: FragmentOutfitsBinding? = null
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var location: Location
+    private lateinit var weatherApi: WeatherApi
+    private var temp: Double = 0.0
+    private var precip: Double = 0.0
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -32,8 +48,47 @@ class OutfitsFragment : Fragment() {
         outfitsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        checkPermission()
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+        println("DEBUG season: " + Util.determineSeason(temp))
         return root
     }
+
+    private fun getLastLocation(){
+        checkPermission()
+        val locationTask : Task<Location> = fusedLocationProviderClient.lastLocation
+
+        locationTask.addOnSuccessListener { OnSuccessListener<Location> {
+            location = it
+            getWeather()
+        }}
+    }
+
+    private fun getWeather(){
+        weatherApi = WeatherApi()
+        weatherApi.getWeather(location)
+        temp = weatherApi.getTemp()
+        precip = weatherApi.getPrecip()
+    }
+
+
+    private fun checkPermission() {
+        if (ContextCompat.checkSelfPermission(requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION) !=
+            PackageManager.PERMISSION_GRANTED) ActivityCompat.requestPermissions(requireActivity(), arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION), 0)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        requireActivity().onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 0) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getLastLocation()
+            }
+        }
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
