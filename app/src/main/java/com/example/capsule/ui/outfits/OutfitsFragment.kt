576f5b0be4e57c963b9b53ee4ee577cd401edc7b
Denzel Tjokroardi <dtjokroa@gmail.com>
2022-11-29 02:06:36 -0800

    revised WeatherApi.kt and OutfitsFragment.kt to function properly

--- app/src/main/java/com/example/capsule/ui/outfits/OutfitsFragment.kt
+++ app/src/main/java/com/example/capsule/ui/outfits/OutfitsFragment.kt
@@ -1,8 +1,9 @@
 package com.example.capsule.ui.outfits
 
 import android.Manifest
+import android.content.Context.LOCATION_SERVICE
 import android.content.pm.PackageManager
-import android.location.Location
+import android.location.*
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -15,19 +16,17 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.capsule.Util
 import com.example.capsule.api.WeatherApi
 import com.example.capsule.databinding.FragmentOutfitsBinding
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.tasks.OnSuccessListener
-import com.google.android.gms.tasks.Task
 
-class OutfitsFragment : Fragment() {
+
+class OutfitsFragment : Fragment(), LocationListener{
 
     private var _binding: FragmentOutfitsBinding? = null
-    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
-    private lateinit var location: Location
+    private lateinit var lastLocation: Location
     private lateinit var weatherApi: WeatherApi
+    private lateinit var locationManager: LocationManager
+    private lateinit var outfitsViewModel: OutfitsViewModel
     private var temp: Double = 0.0
-    private var precip: Double = 0.0
+    private lateinit var season: String
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -38,57 +37,55 @@ class OutfitsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val outfitsViewModel =
-            ViewModelProvider(this).get(OutfitsViewModel::class.java)
+        outfitsViewModel = ViewModelProvider(this)[OutfitsViewModel::class.java]
 
         _binding = FragmentOutfitsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        weatherApi = WeatherApi()
         val textView: TextView = binding.textHome
         outfitsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
-
+        outfitsViewModel.temp.observe(viewLifecycleOwner) {
+            temp = it - 273.15
+            season = Util.determineSeason(temp)
+            println("DEBUG TEMP IS: " + temp.toString())
+            println("DEBUG SEASON IS: " + season)
+        }
         checkPermission()
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity())
-        println("DEBUG season: " + Util.determineSeason(temp))
+        initLocationManager()
         return root
     }
 
-    private fun getLastLocation(){
-        checkPermission()
-        val locationTask : Task<Location> = fusedLocationProviderClient.lastLocation
 
-        locationTask.addOnSuccessListener { OnSuccessListener<Location> {
-            location = it
-            getWeather()
-        }}
-    }
-
-    private fun getWeather(){
-        weatherApi = WeatherApi()
-        weatherApi.getWeather(location)
-        temp = weatherApi.getTemp()
-        precip = weatherApi.getPrecip()
-    }
-
-
-    private fun checkPermission() {
+    private fun checkPermission(){
         if (ContextCompat.checkSelfPermission(requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION) !=
             PackageManager.PERMISSION_GRANTED) ActivityCompat.requestPermissions(requireActivity(), arrayOf(
             Manifest.permission.ACCESS_FINE_LOCATION), 0)
     }
 
-    @Deprecated("Deprecated in Java")
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        requireActivity().onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == 0) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                getLastLocation()
+
+    private fun initLocationManager() {
+        try {
+            locationManager = requireActivity().getSystemService(LOCATION_SERVICE) as LocationManager
+            val criteria = Criteria()
+            criteria.accuracy = Criteria.ACCURACY_FINE
+            val provider: String? = locationManager.getBestProvider(criteria, true)
+            if(provider != null) {
+                val location = locationManager.getLastKnownLocation(provider)
+                if (location != null)
+                    onLocationChanged(location)
             }
+        } catch (e: SecurityException) {
+            println("location manager failed to initialise")
         }
     }
 
+    override fun onLocationChanged(location: Location) {
+        lastLocation = location
+        weatherApi.getWeatherTemp(lastLocation, outfitsViewModel)
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
