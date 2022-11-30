332e355a3750a87e4137abeb98fbe031a229c09a
Denzel Tjokroardi <dtjokroa@gmail.com>
2022-11-29 23:57:02 -0800

    added case if season is fall

--- app/src/main/java/com/example/capsule/ui/outfitHistory/OutfitsHistoryListAdapter.kt
+++
@@ -1,7 +0,0 @@
-package com.example.capsule.ui.outfitHistory
-
-import android.content.Context
-import com.example.capsule.model.ClothingHistory
-
-class OutfitsHistoryListAdapter(private var context: Context, private var outfitList: List<ClothingHistory> ) {
-}
