a70fe6cb59394dfb9093b725e72bbc2eb5455e66
Denzel Tjokroardi <dtjokroa@gmail.com>
2022-12-03 07:39:30 -0800

    rebased to dev

--- app/src/main/java/com/example/capsule/ui/outfitSuggestion/OutfitSuggestionViewModel.kt
+++ app/src/main/java/com/example/capsule/ui/outfitSuggestion/OutfitSuggestionViewModel.kt
@@ -1,36 +1,69 @@
 package com.example.capsule.ui.outfitSuggestion
 
-import androidx.lifecycle.*
+import android.location.Location
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.capsule.api.WeatherApi
 import com.example.capsule.database.Repository
+import com.example.capsule.model.Clothing
 import com.example.capsule.model.ClothingHistory
-import java.lang.IllegalArgumentException
+import kotlinx.coroutines.flow.take
+import kotlinx.coroutines.flow.takeWhile
+import kotlinx.coroutines.runBlocking
 
-class OutfitSuggestionViewModel(private val repository: Repository, private val season: String) : ViewModel() {
+class OutfitSuggestionViewModel(private val repository: Repository) : ViewModel(){
 
+    private val _season = MutableLiveData<String>()
+    val season : LiveData<String>
+    get(){
+        return _season
+    }
 
-    private val _suggestedTopLiveData = repository.suggestedClothingByCategoryForSeason("Tops", season).asLiveData()
+    private val _suggestedTopLiveData = MutableLiveData<Clothing>()
     val suggestedTopLiveData = _suggestedTopLiveData
 
-    private val _suggestedBottomLiveData = repository.suggestedClothingByCategoryForSeason("Bottoms", season).asLiveData()
+    private val _suggestedBottomLiveData = MutableLiveData<Clothing>()
     val suggestedBottomLiveData = _suggestedBottomLiveData
 
-    private val _suggestedOuterwearLiveData = repository.suggestedClothingByCategoryForSeason("Outerwear", season).asLiveData()
+    private val _suggestedOuterwearLiveData =  MutableLiveData<Clothing>()
     val suggestedOuterwearLiveData = _suggestedOuterwearLiveData
 
-    private val _suggestedShoesLiveData = repository.suggestedClothingByCategoryForSeason("Shoes", season).asLiveData()
+    private val _suggestedShoesLiveData =  MutableLiveData<Clothing>()
     val suggestedShoesLiveData = _suggestedShoesLiveData
 
     fun insert(clothingHistoryEntry: ClothingHistory) {
         repository.insertClothingHistory(clothingHistoryEntry)
     }
+
+    fun updateSeason(location: Location, weatherApi: WeatherApi){
+        var chosenSeason: String
+        runBlocking {
+            chosenSeason = weatherApi.getWeatherTemp(location)
+            _season.value = chosenSeason
+            repository.suggestedClothingByCategoryForSeason("Tops", chosenSeason).takeWhile { it.season == chosenSeason }.take(1).collect {
+                _suggestedTopLiveData.value = it
+            }
+            repository.suggestedClothingByCategoryForSeason("Bottoms", chosenSeason).take(1).collect {
+                _suggestedBottomLiveData.value = it
+            }
+            repository.suggestedClothingByCategoryForSeason("Shoes", chosenSeason).take(1).collect {
+                _suggestedShoesLiveData.value = it
+            }
+            repository.suggestedClothingByCategoryForSeason("Outerwear", chosenSeason).take(1).collect {
+                _suggestedOuterwearLiveData.value = it
+            }
+        }
+    }
 }
 
 
 
-class OutfitSuggestionViewModelFactory (private val repository: Repository, private val season: String) : ViewModelProvider.Factory {
-    override fun<T: ViewModel> create(modelClass: Class<T>) : T{ //create() creates a new instance of the modelClass
+class OutfitSuggestionViewModelFactory (private val repository: Repository) : ViewModelProvider.Factory {
+    override fun<T: ViewModel> create(modelClass: Class<T>) : T{ // create() creates a new instance of the modelClass
         if(modelClass.isAssignableFrom(OutfitSuggestionViewModel::class.java))
-            return OutfitSuggestionViewModel(repository, season) as T
-        throw IllegalArgumentException("Unknown ViewModel class")
+            return OutfitSuggestionViewModel(repository) as T
+        throw IllegalArgumentException("DEBUG Unknown ViewModel class")
     }
 }
